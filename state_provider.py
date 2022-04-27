82269e64e94aa5699c22f0376d53dc5e520f9a48
Josh Lloyd <834342+nevercast@users.noreply.github.com>
2022-04-28 00:20:51 +1200

    Provide total data helper

--- state_provider.py
+++ state_provider.py
@@ -22,8 +22,8 @@ def get_replay_batch(gamemode, batch_size, verbose=False):
     print(expression)
   cur.execute(expression)
   results = cur.fetchall()
-  if len(results) < batch_size:
-    # print('WARNING: Batch size {} is larger than the number of rows in the table {}'.format(batch_size, gamemode))
+  if 0 < len(results) < batch_size:
+    print('WARNING: Batch size {} is larger than the number of rows ({}) in the table {}, making multiple requests.'.format(batch_size, len(results), gamemode))
     results += get_replay_batch(gamemode, batch_size - len(results))
   return results
 
@@ -42,12 +42,21 @@ def get_random_play_sequence(gamemode, batch_size, verbose=False):
   cur.execute(expression)
   return cur.fetchall()
 
+def get_total_data_count(gamemode):
+  """ Get the total number of rows in the table """
+  if gamemode not in GAMEMODE_PLAYERS:
+    raise ValueError('Invalid gamemode {}'.format(gamemode))
+  cur = con.cursor()
+  cur.execute(f'''select count(*) from {gamemode};''')
+  return cur.fetchone()[0]
+
 # Main test
 if __name__ == '__main__':
   test_batch_size = 5000
   for gamemode in GAMEMODE_PLAYERS:
-    print(gamemode)
     batch, sequence = len(get_replay_batch(gamemode, 5000)), len(get_random_play_sequence(gamemode, 5000))
     assert test_batch_size == batch, 'Replay batch and test batch sizes do not match for {}, got: {}'.format(gamemode, batch)
     assert test_batch_size == sequence, 'Replay sequence and test batch sizes do not match for {}, got: {}'.format(gamemode, sequence)
-    print('OK')
+    # Print the gamemode, the total data count for that game mode, and "OK" if the sizes match
+    # Format the total data count with commas for thousands and spacing for 9 digits
+    print('{} ({}): {}'.format(gamemode, '{:,}'.format(get_total_data_count(gamemode)), 'OK' if batch == sequence else 'FAIL'))
