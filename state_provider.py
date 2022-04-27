3ac90bdcdeb5c02560a027698f4bf28f12a96336
Josh Lloyd <834342+nevercast@users.noreply.github.com>
2022-04-28 02:40:58 +1200

    state provider supports suffix data tables

--- state_provider.py
+++ state_provider.py
@@ -8,14 +8,18 @@ GAMEMODE_PLAYERS = {
   'ssl_3v3': 6,
 }
 
-def get_replay_batch(gamemode, batch_size, verbose=False):
+DATA_TABLE_SUFFIXES = ['train', 'validation', 'test']
+
+def get_replay_batch(gamemode, suffix, batch_size, verbose=False):
   if gamemode not in GAMEMODE_PLAYERS:
     raise ValueError('Invalid gamemode {}'.format(gamemode))
+  if suffix not in DATA_TABLE_SUFFIXES:
+    raise ValueError('Invalid suffix {}'.format(suffix))
   cur = con.cursor()
   columns = get_table_columns(GAMEMODE_PLAYERS[gamemode])
   expression =f'''
-  select {','.join(columns)} from {gamemode} where rowid in (
-      select distinct (1+abs(random()) % (SELECT rowid FROM {gamemode} ORDER BY rowid DESC LIMIT 1)) from {gamemode} limit {batch_size}
+  select {','.join(columns)} from {gamemode}_{suffix} where rowid in (
+      select distinct (1+abs(random()) % (SELECT rowid FROM {gamemode}_{suffix} ORDER BY rowid DESC LIMIT 1)) from {gamemode}_{suffix} limit {batch_size}
   );
   '''
   if verbose:
@@ -27,14 +31,16 @@ def get_replay_batch(gamemode, batch_size, verbose=False):
     results += get_replay_batch(gamemode, batch_size - len(results))
   return results
 
-def get_random_play_sequence(gamemode, batch_size, verbose=False):
+def get_random_play_sequence(gamemode, suffix, batch_size, verbose=False):
   if gamemode not in GAMEMODE_PLAYERS:
     raise ValueError('Invalid gamemode {}'.format(gamemode))
+  if suffix not in DATA_TABLE_SUFFIXES:
+    raise ValueError('Invalid suffix {}'.format(suffix))
   cur = con.cursor()
   columns = get_table_columns(GAMEMODE_PLAYERS[gamemode])
   expression =f'''
-  select {','.join(columns)} from {gamemode} limit {batch_size} offset (
-      select (1+abs(random()) % ((SELECT rowid FROM {gamemode} ORDER BY rowid DESC LIMIT 1) - {batch_size})) from {gamemode} limit 1
+  select {','.join(columns)} from {gamemode}_{suffix} limit {batch_size} offset (
+      select (1+abs(random()) % ((SELECT rowid FROM {gamemode}_{suffix} ORDER BY rowid DESC LIMIT 1) - {batch_size})) from {gamemode}_{suffix} limit 1
   );
   '''
   if verbose:
@@ -42,21 +48,24 @@ def get_random_play_sequence(gamemode, batch_size, verbose=False):
   cur.execute(expression)
   return cur.fetchall()
 
-def get_total_data_count(gamemode):
+def get_total_data_count(gamemode, suffix):
   """ Get the total number of rows in the table """
   if gamemode not in GAMEMODE_PLAYERS:
     raise ValueError('Invalid gamemode {}'.format(gamemode))
+  if suffix not in DATA_TABLE_SUFFIXES:
+    raise ValueError('Invalid suffix {}'.format(suffix))
   cur = con.cursor()
-  cur.execute(f'''select count(*) from {gamemode};''')
+  cur.execute(f'''select count(*) from {gamemode}_{suffix};''')
   return cur.fetchone()[0]
 
 # Main test
 if __name__ == '__main__':
   test_batch_size = 5000
   for gamemode in GAMEMODE_PLAYERS:
-    batch, sequence = len(get_replay_batch(gamemode, 5000)), len(get_random_play_sequence(gamemode, 5000))
-    assert test_batch_size == batch, 'Replay batch and test batch sizes do not match for {}, got: {}'.format(gamemode, batch)
-    assert test_batch_size == sequence, 'Replay sequence and test batch sizes do not match for {}, got: {}'.format(gamemode, sequence)
-    # Print the gamemode, the total data count for that game mode, and "OK" if the sizes match
-    # Format the total data count with commas for thousands and spacing for 9 digits
-    print('{} ({}): {}'.format(gamemode, '{:,}'.format(get_total_data_count(gamemode)), 'OK' if batch == sequence else 'FAIL'))
+    for suffix in DATA_TABLE_SUFFIXES:
+      batch, sequence = len(get_replay_batch(gamemode, suffix, 5000)), len(get_random_play_sequence(gamemode, suffix, 5000))
+      assert test_batch_size == batch, 'Replay batch and test batch sizes do not match for {}_{}, got: {}'.format(gamemode, suffix, batch)
+      assert test_batch_size == sequence, 'Replay sequence and test batch sizes do not match for {}_{}, got: {}'.format(gamemode, suffix, sequence)
+      # Print the gamemode, the total data count for that game mode, and "OK" if the sizes match
+      # Format the total data count with commas for thousands and spacing for 9 digits
+      print('{}_{} ({}): {}'.format(gamemode, suffix, '{:,}'.format(get_total_data_count(gamemode, suffix)), 'OK' if batch == sequence else 'FAIL'))
