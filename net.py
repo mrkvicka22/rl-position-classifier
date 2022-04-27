564222ed40ade585074f4ef88db02aa59334d135
Josh Lloyd <834342+nevercast@users.noreply.github.com>
2022-04-28 03:36:47 +1200

    x, y, z net, with spicy sigmoid

--- net.py
+++ net.py
@@ -3,17 +3,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # input
-# ball x,y
-# prediction x,y
-# team mate x,y
-# opponent x,y
-# opponent x,y
+# ball x,y,z
+# prediction x,y,z
+# team mate x,y,z
+# opponent x,y,z
+# opponent x,y,z
 
 # output
 # prediction is correct probability: j
 # set to one when using training data (since prediction will be from the data frame)
 # set to zero when using false data (since prediction will be from random generation)
 
+n_dims = 3 # 3d
 
 class Net(nn.Module):
   def __init__(self, layers):
@@ -23,23 +24,23 @@ class Net(nn.Module):
   def forward(self, x):
     return self.model(x)
 
-def create_network(player_count):
-  """ Create a network based on player count (changes network size) """
+def create_model(player_count):
+  """ Create a model based on player count (changes network size) """
   return Net([
-    # player (x,y) + ball (x,y)
-    nn.Linear(player_count * 2 + 2, 128), nn.ReLU(),
+    # player (x,y,z) + ball (x,y,z)
+    nn.Linear(player_count * n_dims + n_dims, 128), nn.ReLU(),
     nn.Linear(128,128), nn.ReLU(),
     nn.Linear(128,128), nn.ReLU(),
-    nn.Linear(128,1)
+    nn.Linear(128,1), nn.Sigmoid(),
   ])
 
 def dead_test():
   print("This test only checks that the network works, not that it produces correct results.")
   for player_size in (2, 4, 6):
-    net = create_network(player_size)
+    net = create_model(player_size)
     net.eval()
     with torch.no_grad():
-      test_data = torch.rand( player_size * 2 + 2)
+      test_data = torch.rand( player_size * n_dims + n_dims)
       inferred = net(test_data)
       print(f"{player_size} player prediction: {inferred}")
 
