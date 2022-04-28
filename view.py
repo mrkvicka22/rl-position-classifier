899717c22e175019a71d8f806cc7d402c25ffe41
Josh Lloyd <834342+nevercast@users.noreply.github.com>
2022-04-28 17:44:18 +1200

    Add volumetric scatter for 3d view

--- view.py
+++ view.py
@@ -1,69 +1,82 @@
+import matplotlib
+import matplotlib.cm as cmx
+from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import torch
 from torch import load, sigmoid
+import train
 import state_provider
+from mpl_toolkits.mplot3d.art3d import juggle_axes
 
-MODEL_PATH = "model_7560000.pt"
+MODEL_PATH = "model_ssl_2v2_latest.pt"
 
-_normal_2v2_batch = [4096, 6000] * 5 # 8192, 10240 (compensate for goal depth)
 
 def get_state_batch(batch_size):
-  batch = np.array(state_provider.get_random_play_sequence('ssl_2v2', 'test', batch_size, use_2d_map=True)) / _normal_2v2_batch
-  ball, player_one, player_two, opponent_1, opponent_2 = batch[:, :2], batch[:, 2:4], batch[:, 4:6], batch[:, 6:8], batch[:, 8:10]
-  return np.concatenate([ball, player_two, opponent_1, opponent_2], axis=1), player_one
+  normal = train.normalization(4, use_2d_map=False)
+  return np.array(state_provider.get_random_play_sequence('ssl_2v2', 'test', batch_size, use_2d_map=False)) / normal
 
-def get_map(model, game_state):
-  ball, player_two, opponent_1, opponent_2 = game_state[:2], game_state[2:4], game_state[4:6], game_state[6:8]
-  row = np.concatenate([ball, [0, 0], player_two, opponent_1, opponent_2], axis=0)
-  leftright, updown = 410, 600
-  batch = np.full((410*600, 10), row)
-  batch[:, 2:4] = np.array([[(x - 205) / 410, (y - 300) / 600] for x in range(leftright) for y in range(updown)])
-  return sigmoid(model(torch.from_numpy(batch).float())).view(410, 600).numpy()
+def generate_volume_points(width_steps, depth_steps, height_steps):
+  return np.linspace(-1, 1, width_steps), np.linspace(-1, 1, depth_steps), np.linspace(0, 1, height_steps)
+
+def generate_volume_batch(width_steps, depth_steps, height_steps):
+  xs, ys, zs = generate_volume_points(width_steps, depth_steps, height_steps)
+  return np.array([[x, y, z] for x in xs for y in ys for z in zs])
+
+def infer_point_cloud_from_volume(volume, volume_space_shape, game_state, model):
+  # point replaces game_state[3:6] for every point in volume
+  # volume is a numpy array of points
+  x = np.array([np.concatenate([game_state[0:3], point, game_state[6:]]) for point in volume])
+  # Pass x through torch model (10, 10, 10, 15) -> (10, 10, 10, 1)
+  y = model(torch.from_numpy(x).float())
+  return sigmoid(y).reshape(volume_space_shape + (1,)).numpy()
+
 
 def dead_test():
 
-  fig, ax = plt.subplots()
+  # cm = plt.get_cmap()
+  cm = LinearSegmentedColormap.from_list('', ['white', 'white', 'red'])
+  cNorm = matplotlib.colors.Normalize(vmin=-1, vmax=1)
+  scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
+  fig = plt.figure()
+  ax = Axes3D(fig)
+  ax.set_xlabel('X')
+  ax.set_ylabel('Y')
+  ax.set_zlabel('Z')
 
-  # Create numpy array from list
-  image_ref = None
-  tests, label = get_state_batch(45 * 30)
-  self_circle = plt.Circle((0, 0), 10, color='c')
-  ball_circle = plt.Circle((0, 0), 10, color='w')
-  team_player_circle = plt.Circle((0, 0), 10, color='b')
-  opponent_1_circle = plt.Circle((0, 0), 10, color='y')
-  opponent_2_circle = plt.Circle((0, 0), 10, color='y')
-
-  ax.add_artist(self_circle)
-  ax.add_artist(ball_circle)
-  ax.add_artist(team_player_circle)
-  ax.add_artist(opponent_1_circle)
-  ax.add_artist(opponent_2_circle)
-
+  states = get_state_batch(45 * 30)[::3, :]
   model = load(MODEL_PATH)
   model.eval()
 
+  size = 20
+  points = generate_volume_batch(size, size, size)
+
+  scatter_ref = None
+  entity_ref = None
+
   with torch.no_grad():
-    for test, label in zip(tests, label):
-      test = np.array(test)
-      ball_pos = test[0:2] * [205, 300] + [205, 300]
-      team_player_pos = test[2:4] * [205, 300] + [205, 300]
-      opponent_1_pos = test[4:6] * [205, 300] + [205, 300]
-      opponent_2_pos = test[6:8] * [205, 300] + [205, 300]
-      self_circle.center = label * [205, 300] + [205, 300]
-      ball_circle.center = ball_pos
-      team_player_circle.center = team_player_pos
-      opponent_1_circle.center = opponent_1_pos
-      opponent_2_circle.center = opponent_2_pos
-      npimg = get_map(model, torch.from_numpy(test).float())
-      # plt.imsave(f'test_{idx}.png', npimg, cmap="Greys")
-      # Draw the image
-      if image_ref is None:
-        image_ref = ax.imshow(npimg, cmap="plasma")
-      else:
-        image_ref.set_data(npimg)
-      fig.canvas.flush_events()
-      plt.pause(1 / 30.0)
+    while True:
+      for test_state in states:
+        # Get point cloud from volume
+        point_cloud = infer_point_cloud_from_volume(points, (size, size, size), test_state, model)
+
+        entity_positions = [
+          test_state[0::3],
+          test_state[1::3],
+          test_state[2::3]
+        ]
+
+        # Draw point cloud
+        if entity_ref is None:
+          entity_ref = ax.scatter(entity_positions[0], entity_positions[1], entity_positions[2], c=['black', 'cyan', 'blue', 'orange', 'orange'], s=20)
+          scatter_ref = ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=point_cloud[:, :, :, 0].reshape((-1)), cmap=cm, s=1)
+          fig.colorbar(scalarMap)
+        else:
+          scatter_ref.set_array(point_cloud[:, :, :, 0].reshape((-1)))
+          entity_ref._offsets3d = tuple(entity_positions)
+          fig.canvas.draw()
+          plt.pause(1 / 30)
 
 if __name__ == '__main__':
   def __main__():
