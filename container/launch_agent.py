0f042209f78ba6af68259064fb519c8e60aeb4ea
Josh Lloyd <834342+nevercast@users.noreply.github.com>
2022-05-01 23:25:11 +1200

    Container changes

--- container/launch_agent.py
+++ container/launch_agent.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from time import sleep
 import os
 import uuid
@@ -14,14 +15,13 @@ if __name__ == "__main__":
   wandb.init(entity='nevercast', project='rl-position-classifier', name=unique_name)
   api = wandb.Api()
   while True:
-    print("Checking that the artifacts are ready...")
-    wandb.run.use_artifact("nevercast/rl-position-classifier/db-6m-ssl:latest").download("/workspace")
-    sweeps = api.project('rl-position-classifier').sweeps()
-    if len(sweeps) == 0:
-      print("No sweeps found, waiting...")
-      sleep(120)
-      exit(0)
-    begin_sweep = random.choice(sweeps)
+    # sweeps = api.project('rl-position-classifier').sweeps()
+    # if len(sweeps) == 0:
+    #   print("No sweeps found, waiting...")
+    #   sleep(120)
+    #   exit(0)
+    # begin_sweep = random.choice(sweeps)
+    begin_sweep = (namedtuple('Sweep', 'id')('t1r9kp3l'))
     print(f'Starting agent to service sweep/{begin_sweep.id}, will service at least 10 runs...')
     wandb.agent(begin_sweep.id, entity='nevercast', project='rl-position-classifier', count=10)
     print('Agent ended, restarting in 5 seconds...')
--- container/startup.sh
+++ container/startup.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 set -e
 
+apt-get install -y python3-pip git python-is-python3
+cd /workspace
+
+cp /workspace/.netrc ~/.netrc
+
+# If rl-position-classifier is not installed, install it
+if [ ! -d "/workspace/rl-position-classifier" ]; then
+    git clone https://github.com/nevercast/rl-position-classifier.git --depth 1
+fi
+
+python -m pip install -r /workspace/rl-position-classifier/requirements.txt
+
 while true; do
   cd /workspace/rl-position-classifier
   git pull --ff-only
-  WANDB_SILENT=true python3 ../launch_agent.py
+  WANDB_SILENT=true python ../launch_agent.py
   echo "Restarting in 5 seconds..."
   sleep 5
 done
